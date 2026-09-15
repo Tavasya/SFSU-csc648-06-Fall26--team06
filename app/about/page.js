565f2ffe03add7370b123ce384e9a593282def2e
gitphongn <phonnguyen10@gmail.com>
2026-09-15 15:03:01 -0700

    added phong profile

--- app/about/page.js
+++ app/about/page.js
@@ -43,11 +43,11 @@ const TEAM_MEMBERS = [
   {
     image: "/images/member5.jpg",
     name: "Team Member 5",
-    role: "place holder",
+    role: "Database Engineer",
     modalRole: "Database Engineer",
-    bio: "Bio description goes here.",
-    github: "https://github.com/USERNAME",
-    linkedin: "https://discord.com/users/USERID",
+    bio: "I am a current 4th year at SFSU studying computer science. I enjoy working on cars and is from San Francisco.",
+    github: "https://github.com/gitphongn",
+    linkedin: "https://www.linkedin.com/in/phong-nguyen-132868284/",
   },
   {
     image: "/images/member6.jpg",
